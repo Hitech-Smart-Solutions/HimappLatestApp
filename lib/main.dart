4ececa721b9818136d734fc969e325a0691d00d2
CoderKing2705 <soumy.oza@hitechsmartsolutions.com>
2025-01-24 19:40:37 +0530

    Second commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   // This widget is root of my application
   @override
   Widget build(BuildContext context) {
