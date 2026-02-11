5c8edcc16d5348d3474f46f01e0fb5007c865fa9
solankihardik96 <hardik.solanki@hitechsmartsolutions.com>
2026-02-11 16:07:44 +0530

    Link Test

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,7 +6,7 @@ class ScreenTypes {
 
 class AppSettings {
   static const String url =
-      'https://s3.ap-south-1.amazonaws.com/documents.himapp.live'; //Production
+      'https://s3.ap-south-1.amazonaws.com/documents.himapp.test'; //Production
   static const Map<String, int> DEVICEID = {
     'Web': 1,
     'Mobile': 2,
